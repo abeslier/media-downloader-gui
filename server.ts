76ce008836f6be0695ee818537a4b20e0e9eccc9
abeslier <Adrien.Beslier@ulb.be>
2025-03-03 12:53:15 +0100

    serve index.html

--- server.ts
+++ server.ts
@@ -1,5 +1,17 @@
-function handler(req: Request): Response {
-  return new Response("req.url = " + req.url);
+async function handler(_req: Request): Promise<Response> {
+    try {
+        const file = await Deno.open("index.html");
+        return new Response(file.readable, {
+            headers: {
+                "content-type": "text/html; charset=utf-8"
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response("404 Not Found", {
+            status: 404
+        });
+    }
 }
 
 const _server = Deno.serve({  // https://docs.deno.com/api/deno/~/Deno.serve
