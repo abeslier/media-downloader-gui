d4dbe757dcb1c09677bd9ca41dd1144735da2e25
abeslier <Adrien.Beslier@ulb.be>
2025-03-03 16:31:07 +0100

    file server

---
+++ public/script.js
@@ -0,0 +1,9 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const urlInput = document.getElementById('urlInput');
+
+    if (!urlInput.value.trim()) {
+        urlInput.classList.add('error');
+    } else {
+        urlInput.classList.remove('error');
+    }
+});
--- server.ts
+++ server.ts
@@ -1,17 +1,9 @@
-async function handler(_req: Request): Promise<Response> {
-    try {
-        const file = await Deno.open("index.html");
-        return new Response(file.readable, {
-            headers: {
-                "content-type": "text/html; charset=utf-8"
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return new Response("404 Not Found", {
-            status: 404
-        });
-    }
+import { serveDir } from "@std/http/file-server";  // `deno add jsr:@std/http`
+
+async function handler(req: Request): Promise<Response> {
+    return await serveDir(req, {  // https://jsr.io/@std/http/doc/~/serveDir
+        fsRoot: "public",
+    });
 }
 
 const _server = Deno.serve({  // https://docs.deno.com/api/deno/~/Deno.serve
