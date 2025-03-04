6c823cb161d5beea7f0553d189af01444f58150e
abeslier <Adrien.Beslier@ulb.be>
2025-03-05 00:10:56 +0100

    working url download from web ui

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import { serveDir } from "@std/http/file-server"; // `deno add jsr:@std/http`
+import { download } from "./download.ts";
+
+const ffmpegLocationLinux: string = "ffmpeg-master-latest-linux64-gpl/bin/";
 
 async function handler(req: Request): Promise<Response> {
     const url = new URL(req.url);
 
     if (url.pathname == "/api/download" && req.method == "POST") {
-        console.log(await req.json());
+        const { mediaUrl } = await req.json();
+
+        for await (const progress of download(ffmpegLocationLinux, mediaUrl)) {
+            console.log(`${progress}`);
+        }
+
         return new Response();
     }
     return await serveDir(req, { // https://jsr.io/@std/http/doc/~/serveDir
