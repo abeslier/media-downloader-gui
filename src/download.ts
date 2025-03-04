e99a8a82b9435472f6865949e21f53cde45138f2
abeslier <Adrien.Beslier@ulb.be>
2025-03-04 20:41:09 +0100

    download command std piping

--- src/download.ts
+++ src/download.ts
@@ -1,16 +1,33 @@
 import { isCommandOnPath } from "./subprocess.ts";
+import { extractPercentage } from "./utils.ts";
 
 export async function download(ffmpegLocation: string, url: string) {
     if (await isCommandOnPath("yt-dlp")) {
-        const command = new Deno.Command("yt-dlp", {
+        const command = new Deno.Command("yt-dlp", { // TODO try-catch
             args: [
                 "--ffmpeg-location",
                 ffmpegLocation,
                 url,
             ],
+            stdout: "piped",
+            stderr: "piped",
         });
+        const subprocess = command.spawn();
 
-        await command.output();
+        const stream = subprocess.stdout; // TODO stderr
+        const reader = stream.getReader();
+        const decoder = new TextDecoder();
+        while (true) {
+            const { value, done } = await reader.read();
+            const progress = extractPercentage(decoder.decode(value));
+            if (progress !== null) {
+                console.log(`${progress}`);
+            }
+            if (done) break;
+        }
+
+        const { code } = await subprocess.status;
+        console.log(`yt-dlp exit code : ${code}`);
     } else {
         console.error("yt-dlp not on PATH");
     }
